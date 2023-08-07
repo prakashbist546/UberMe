fe6404c29cb65c7e9d9849a4d88d4d0dc74a9c61
Prakash Bist <prakash.bist@smartsolar.com.np>
2023-08-07 10:31:54 +0900

    Initial Commit

---
+++ UberMe/UberMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberMeApp.swift
+//  UberMe
+//
+//  Created by Prakash Bist on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct UberMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
